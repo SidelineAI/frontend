2c06a35d23a3018e29b0ffc37f119bb4cf34fc4a
Mingkuan Yan <mingkuany@gmail.com>
2024-03-28 23:22:02 -0400

    Search page 1st draft complete!

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,10 +1,52 @@
+import styles from "../styles/playercard.module.scss";
+
 export default function PlayerCard({ player }) {
   return (
-    <div className="container">
-      <img src="" alt="Player" />
-      <div className="info">
-        <div className="left"></div>
-        <div className="right"></div>
+    <div className={styles.container}>
+      <img src={player.img} alt="Player PFP" className={styles.img} />
+      <div className={styles.info}>
+        <div className={styles.left}>
+          <div className={styles.top}>
+            <p className={styles.name}>{player.name}</p>
+            <p className={styles.pos}>| {player.pos}</p>
+            <div className={styles.jerseyContainer}>
+              <p className={styles.numSymbol}>#</p>
+              <p className={styles.jersey}>{player.jersey}</p>
+            </div>
+          </div>
+
+          <p className={styles.school}>{player.school}</p>
+          <div className={styles.physical}>
+            <p>{player.height}</p>
+            <p>{player.age}</p>
+            <p>{player.weight}</p>
+          </div>
+          <div className={styles.skills}>
+            {player.skills.map((skill, index) => (
+              <p className="skill" key={index}>
+                {skill}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div>
+            <p className={styles.stat}>{player.ppg}</p>
+            <p className={styles.label}>Points per Game</p>
+          </div>
+          <div>
+            <p className={styles.stat}>{player.rpg}</p>
+            <p className={styles.label}>Rebounds per Game</p>
+          </div>
+          <div>
+            <p className={styles.stat}>{player.apg}</p>
+            <p className={styles.label}>Assists per Game</p>
+          </div>
+          <div>
+            <p className={styles.stat}>{player.fgp}</p>
+            <p className={styles.label}>Field Goal Percentage</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,64 @@
+import styles from "../styles/search.module.scss";
 import Header from "../components/Header";
+import PlayerCard from "../components/PlayerCard";
 
 export default function Search() {
+  const mockPlayers = [
+    {
+      name: "James Cooper",
+      pos: "SG",
+      jersey: 30,
+      school: "Miami University",
+      height: "6'5\"",
+      age: "22 y/o",
+      weight: "200 lbs",
+      skills: ["Dunking", "Handles", "3-Pointers", "Layups"],
+      ppg: 18.5,
+      rpg: 6.2,
+      apg: 4.8,
+      fgp: "45%",
+      img: "https://4kwallpapers.com/images/walls/thumbs_2t/7603.jpg",
+    },
+    {
+      name: "Kaitlin Bird",
+      pos: "SG",
+      jersey: 30,
+      school: "Temple University",
+      height: "6'5\"",
+      age: "22 y/o",
+      weight: "200 lbs",
+      skills: ["Dunking", "Layups"],
+      ppg: 18.5,
+      rpg: 6.2,
+      apg: 4.8,
+      fgp: "45%",
+      img: "https://img.freepik.com/free-photo/young-caucasian-female-basketball-player-team-action-motion-run-isolated-white-background_155003-41811.jpg",
+    },
+    {
+      name: "Austin Rindels",
+      pos: "SG",
+      jersey: 30,
+      school: "Washington University",
+      height: "6'5\"",
+      age: "22 y/o",
+      weight: "200 lbs",
+      skills: ["Dunking", "Handles", "3-Pointers"],
+      ppg: 18.5,
+      rpg: 6.2,
+      apg: 4.8,
+      fgp: "45%",
+      img: "https://4kwallpapers.com/images/walls/thumbs_2t/7594.jpg",
+    },
+  ];
+
   return (
     <>
       <Header></Header>
+      <div className={styles.resultsContainer}>
+        {mockPlayers.map((player, index) => (
+          <PlayerCard player={player} key={index} />
+        ))}
+      </div>
     </>
   );
 }
