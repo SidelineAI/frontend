77596a62bac652a7a776b3ac069ae6437e920598
Mingkuan Yan <mingkuany@gmail.com>
2024-04-02 19:59:45 -0400

    Skill clip plays on hover! (hardcoded)

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import styles from "../styles/playercard.module.scss";
+import dunking from "../assets/dunking.mp4";
+import handles from "../assets/handles.mp4";
+import threepointers from "../assets/threepointer.mp4";
+import layups from "../assets/layups.mp4";
 
 export default function PlayerCard({ player }) {
+  const [videoSrc, setVideoSrc] = useState(null);
+
+  const handleSkillHover = (skill) => {
+    const skillData = player.skills.find((s) => s.name === skill);
+    if (skillData) {
+      setVideoSrc(skillData.video);
+    }
+  };
+
+  const handleSkillLeave = () => {
+    setVideoSrc(null);
+  };
+
   return (
     <div className={styles.container}>
-      <img src={player.img} alt="Player PFP" className={styles.img} />
+      {videoSrc ? (
+        <video autoPlay loop muted playsInline>
+          <source
+            src={
+              videoSrc === "dunking"
+                ? dunking
+                : videoSrc === "handles"
+                ? handles
+                : videoSrc === "threepointers"
+                ? threepointers
+                : videoSrc === "layups"
+                ? layups
+                : ""
+            }
+            type="video/mp4"
+          />
+        </video>
+      ) : (
+        <img src={player.img} alt="Player PFP" className={styles.img} />
+      )}
       <div className={styles.info}>
         <div className={styles.left}>
           <div className={styles.top}>
@@ -23,8 +60,12 @@ export default function PlayerCard({ player }) {
           </div>
           <div className={styles.skills}>
             {player.skills.map((skill, index) => (
-              <p className="skill" key={index}>
-                {skill}
+              <p
+                key={index}
+                onMouseEnter={() => handleSkillHover(skill.name)}
+                onMouseLeave={handleSkillLeave}
+              >
+                {skill.name}
               </p>
             ))}
           </div>
